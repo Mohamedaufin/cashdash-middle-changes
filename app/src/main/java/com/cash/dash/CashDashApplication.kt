a75370bca8569d27a6c8d3ad4b66373ffc8f01ed
Mohamedaufin <mohamedaufin64@gmail.com>
2026-04-08 21:16:23 +0530

    updated copy.... arun changes !

--- app/src/main/java/com/cash/dash/CashDashApplication.kt
+++ app/src/main/java/com/cash/dash/CashDashApplication.kt
@@ -9,8 +9,11 @@ import android.media.AudioAttributes
 import android.media.RingtoneManager
 import android.os.Build
 
+import androidx.appcompat.app.AppCompatDelegate
+
 class CashDashApplication : Application() {
     override fun onCreate() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         super.onCreate()
         
         // Start Security Monitoring globally
