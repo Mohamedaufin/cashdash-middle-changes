6d7c6bc9054e5363ee7989886b5ba0383c450b05
Mohamed <moham@example.com>
2026-05-29 21:08:01 +0530

    feat: reverse history page graphs swipe direction to left-to-right for home and use GestureDetector for accurate taps

--- app/src/main/java/com/cash/dash/HistoryActivity.kt
+++ app/src/main/java/com/cash/dash/HistoryActivity.kt
@@ -140,7 +140,7 @@ class HistoryActivity : ThemedActivity() {
             }
         }
 
-        graph.onSwipeLeftListener = {
+        graph.onSwipeRightListener = {
             val intent = Intent(this, MainActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
             startActivity(intent)
