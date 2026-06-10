a5f226922b4f13ee7046f86d07198bf4fdff4a7b
Mohamed <moham@example.com>
2026-06-10 08:15:12 +0530

    Bump version to 0.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.cash.dash"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.3.1"
+        versionCode = 5
+        versionName = "0.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
