3767272d091991923e94e1fb58a6517afc76880a
Mohamed <moham@example.com>
2026-08-10 16:34:18 +0530

    Bump version code to 20 and version name to 0.4.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.cash.dash"
         minSdk = 26
         targetSdk = 36
-        versionCode = 19
-        versionName = "0.4.6"
+        versionCode = 20
+        versionName = "0.4.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
